1ca48113852698b74eff76d93400a4d25d26cb89
kaji <kajiharalivi@icloud.com>
2022-04-05 15:40:49 +0900

    一から作り直します

--- backend/api/main.go
+++ backend/api/main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/kajikaji0725/ToDo-App/api/pkg/db"
 	"github.com/kajikaji0725/ToDo-App/api/pkg/server"
+	"github.com/rs/cors"
 )
 
 func main() {
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:5000"},
+		AllowCredentials: false,
+		AllowedMethods:   []string{"GET", "POST", "HEAD","DELETE","PUT"},
+		AllowedHeaders:   []string{"Content-type"},
+		MaxAge:           24 * int(time.Hour),
+	})
 	config := db.Config{
 		Host:     "homework-db",
 		Username: "root",
@@ -16,12 +25,13 @@ func main() {
 		DBname:   "root",
 		Port:     "5432",
 	}
-	apiClient ,err := server.NewApiClient(&config)
+	apiClient, err := server.NewApiClient(&config)
 	router := apiClient.NewRouter()
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err := http.ListenAndServe(":8080", router); err != nil {
+	handler := c.Handler(router)
+	if err := http.ListenAndServe(":8080", handler); err != nil {
 		log.Fatal(err)
 	}
 }
--- backend/api/pkg/db/model/ToDo.go
+++ backend/api/pkg/db/model/ToDo.go
@@ -3,7 +3,7 @@ package model
 import "time"
 
 type ToDo struct {
-	Id      string    `json:"id"`
-	Subject string    `json:"subject"`
-	Date    time.Time `json:"date"`
+	ID string    `json:"id"`
+	Subject    string    `json:"subject"`
+	Date       time.Time `json:"date"`
 }
